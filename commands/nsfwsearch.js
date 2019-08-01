d863592495de25f6f158ac6ec5586344f37d130f
Abderrahman Taibi <ataibi@protonmail.com>
2019-08-01 04:23:08 +0200

    add nsfw search command

---
+++ commands/nsfwsearch.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const Pornsearch = require('pornsearch');
+
+module.exports.run = async (bot, message, args) => {
+  const Searcher = new Pornsearch(args, driver = 'pornhub');
+  Searcher.videos()
+    .then(videos => {
+      let lourdeVideo = new Discord.RichEmbed()
+        .setTitle(videos[1].title)
+        .setThumbnail(videos[1].thumb)
+        .addField('Duration :', videos[1].duration)
+        .setURL(videos[1].url)
+        .setColor('#FF9900');
+      message.channel.send(lourdeVideo);
+      console.log(videos);
+    });
+}
+
+module.exports.help = {
+  name: 'porn',
+  description: 'on se met bien, cherche du porn oklm entre potes',
+  examples: 'stp porn midget furry fucks Bob Razowski'
+}
